0bc9d0fb1eab7e6580f5b29e7f75022dbb41fe0e
vicente <isc.luisangel@gmail.com>
2026-07-04 11:49:56 -0500

    chore: initialize iOS project

---
+++ ProntoCheck/ProntoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProntoCheckApp.swift
+//  ProntoCheck
+//
+//  Created by usuario on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ProntoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
